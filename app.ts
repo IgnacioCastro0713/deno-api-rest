97f17966967c38e3193ea2dbd9defe4d96cdad7d
IgnacioCastro0713 <ignaciomenchaca666@gmail.com>
2020-05-23 23:03:19 -0500

    add cors

--- app.ts
+++ app.ts
@@ -1,12 +1,12 @@
-import { oak } from "./dependencies.ts";
+import { oak, cors } from "./dependencies.ts";
 import router from "./router.ts";
 
 const {Application} = oak;
 const app = new Application();
 
 // routes
+app.use(cors.oakCors());
 app.use(router.routes())
 app.use(router.allowedMethods())
-
 
 export default app;
--- dependencies.ts
+++ dependencies.ts
@@ -1,2 +1,3 @@
-export * as mongo from "https://deno.land/x/mongo@v0.7.0/mod.ts";
 export * as oak from "https://deno.land/x/oak/mod.ts";
+export * as mongo from "https://deno.land/x/mongo@v0.7.0/mod.ts";
+export * as cors from "https://deno.land/x/cors/mod.ts";
